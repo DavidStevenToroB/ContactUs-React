155adf537edd1ac642c48f86666a073e7078d198
David Steven T <davidtoro013125@gmail,com>
2025-04-08 18:55:20 -0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://DavidStevenToroB.github.io/Email-React',
+  base: 'https://github.com/DavidStevenToroB/ContactUs-React',
 })
